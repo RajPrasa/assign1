7efbfce5d5a16afba890d8d38e74769962815cf9
RajPrasa <93071444+RajPrasa@users.noreply.github.com>
2022-03-27 23:28:51 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,33 @@
+import React from "react"
+import ReactDOM from "react-dom"
+
+ReactDOM.render(
+    <div>
+        <img src="https://i5.walmartimages.com/asr/594929a8-8cb9-41b6-aec2-a52e73f6867a.182e8423b76c8be28ec20a52ff73aff5.jpeg?odnHeight=612&odnWidth=612&odnBg=FFFFFF"></img><br></br>
+        <a href="">Acer Aspire 5 A515-44G-R83X 15.6" FHD Laptop, AMD Ryzen 5, 8GB RAM, 256GB SSD,<br></br> 
+        Windows 10 Home, Pure Silver, NX.HW6AA.001</a>
+        <h2>Rs.32,000/-</h2>
+    </div>,document.getElementById("root1")
+
+    
+)
+
+ReactDOM.render(
+    <div>
+        <img src="https://m.media-amazon.com/images/I/71rceuYk9JL._SX679_.jpg"></img><br></br>
+        <a href="">Acer Aspire 5 11th Gen Core i5 14-inch (35.56 cms) Full HD IPS Thin & Light Laptop-<br></br> 
+        (8 GB/512 GB SSD/Windows 11 Home/Intel Iris Xe Graphics/Pure Silver/1.45 KG/MS Office), A514-54</a>
+        <h2>Rs.54,000/-</h2>
+    </div>,document.getElementById("root2")
+)
+ReactDOM.render(
+    <div>
+        <img src="https://m.media-amazon.com/images/I/61q6x-ll5FL._SX679_.jpg"></img><br></br>
+        <a href="">Lenovo Ideapad 3 AMD Ryzen 5 5500U 15.6" FHD Thin & Light Laptop <br></br> 
+        (8GB/512GB SSD/Windows 11/Office 2021/Backlit Keyboard/2Yr Warranty/Arctic Grey/1.65Kg), 82KU017KIN</a>
+        <h2>Rs.38,000/-</h2>
+    </div>,document.getElementById("root3")
+)
+
+
+
